773bcf171b81559405e29259e787aaa00b0d10e4
ahs1n <ahsanpervez74@gmail.com>
2021-04-20 11:23:48 +0500

    Working on update information

--- app/src/main/java/com/example/vehiclerecord/fragment/list/ListAdapter.kt
+++ app/src/main/java/com/example/vehiclerecord/fragment/list/ListAdapter.kt
@@ -3,10 +3,15 @@ package com.example.vehiclerecord.fragment.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vehiclerecord.R
 import com.example.vehiclerecord.model.Vehicle
-import kotlinx.android.synthetic.main.custom_row.view.*
+import kotlinx.android.synthetic.main.custom_row.view.driverName_txt
+import kotlinx.android.synthetic.main.custom_row.view.meterIn_txt
+import kotlinx.android.synthetic.main.custom_row.view.meterOut_txt
+import kotlinx.android.synthetic.main.custom_row.view.vehicleNo_txt
+import kotlinx.android.synthetic.main.item_driver_layout.view.*
 
 class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
@@ -31,6 +36,11 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
         holder.itemView.vehicleNo_txt.text = currentItem.vehilceNo
         holder.itemView.meterIn_txt.text = currentItem.meter_in
         holder.itemView.meterOut_txt.text = currentItem.meter_out
+
+        holder.itemView.timeOut.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToUpdateFragment()
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     fun setData(vehicle: List<Vehicle>) {
